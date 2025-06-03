6cdfac32118efbe80277e873c8c16df45909d182
YafiRiifdah <yafilala2@gmail.com>
2025-06-03 20:34:54 +0700

    ganggu bgt favicon

--- src/middleware/notFoundHandler.js
+++ src/middleware/notFoundHandler.js
@@ -1,7 +1,13 @@
 const notFoundHandler = (req, res, next) => {
+  // Cegah error untuk permintaan favicon.ico
+  if (req.originalUrl === '/favicon.ico') {
+    return res.status(204).end();
+  }
+
+  // not found route
   const error = new Error(`Tidak Ditemukan - ${req.originalUrl}`);
   res.status(404);
-  next(error); // Meneruskan error ke errorHandler utama
+  next(error);
 };
 
 module.exports = { notFoundHandler };
